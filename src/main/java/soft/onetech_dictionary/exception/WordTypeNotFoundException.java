77374a5ba26958cebfd64c9e0da3c9dd41a81c8b
adilbek <adilbek.akhmet@gmail.com>
2021-11-13 12:01:14 +0600

    Practice 7

--- src/main/java/soft/onetech_dictionary/exception/WordTypeNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package soft.onetech_dictionary.exception;
-
-public class WordTypeNotFoundException extends RuntimeException {
-
-    public WordTypeNotFoundException(String message) {
-        super(message);
-    }
-}
